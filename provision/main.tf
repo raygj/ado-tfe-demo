c7d071165b7eaf8bf29b382bea902ce5b1d74ca8
Jim Ray <ray.jim@gmail.com>
2019-07-19 17:10:31 -0400

    add provision

---
+++ provision/main.tf
@@ -0,0 +1,19 @@
+# updated to support TF 0.12
+# backward support for TF 0.11
+variable "name" {
+}
+
+resource "random_id" "random" {
+  keepers = {
+    uuid = "${uuid()}"
+  }
+  byte_length = 32
+}
+
+output "random" {
+  value = "${random_id.random.hex}"
+}
+
+output "hello_world" {
+  value = "Hello, ${var.name}"
+}
